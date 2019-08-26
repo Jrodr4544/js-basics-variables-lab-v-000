6efeb2dc4e8d88b6860910538ec76877b8d68ea0
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2019-08-26 15:02:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // Code your solution in this file!
+<<<<<<< HEAD
 
 const companyName = 'Scuber';
 
 const companyCeo = 'Susan Smith';
 
 let mostProfitableNeighborhood = 'Chelsea';
+=======
+let companyName = 'Scuber';
+const companyName = 'Scuber';
+>>>>>>> 2accce768381129684f9e9e22f30c71920bea7ea
